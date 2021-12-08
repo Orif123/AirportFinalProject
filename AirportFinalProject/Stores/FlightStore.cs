e6b1449b5a84f9ba6f091df00d0d2cb943b8b9d6
Orif123 <ariri25@gmail.com>
2021-12-08 20:37:08 +0200

    Backend

---
+++ AirportFinalProject/Stores/FlightStore.cs
@@ -0,0 +1,25 @@
+﻿using Airport.Data;
+using Airport.Models;
+using AirportFinalProject.Data;
+using AirportFinalProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirportFinalProject.Stores
+{
+    public class FlightStore
+    {
+        private readonly ContextFactory _context;
+        private readonly List<Flight> _flights;
+        public IEnumerable<Flight> Flights => _flights;
+        public FlightStore()
+        {
+            _flights = new List<Flight>();
+        }
+        public void LoadArrivels()
+        {
+
+        }
+    }
+}
